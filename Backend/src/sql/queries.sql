402202473f9a7ab613a9ebb1b3e33cc1514c20b3
Diegomjx <19002145@galileo.edu>
2022-08-25 02:32:38 -0600

    base de datos

--- Backend/src/sql/queries.sql
+++ Backend/src/sql/queries.sql
@@ -1,1 +1,30 @@
--- CREATE DATABASE Manual;   
+
+-- CREATE DATABASE Manual;  
+---USE Manual
+---Perfil---
+CREATE TABLE Perfil (
+	ID bigint identity(1,123) primary key,
+	USUARIO varchar(Max),
+	CONTRASEÑA varchar(Max),
+	NOMBRE varchar(Max), 
+	CORREO varchar(Max),
+
+);
+---Listas---
+---Historial---
+---Apuntes---
+---Seguidores---
+
+
+/*
+DROP TABLE Perfil;  
+DROP TABLE Apuntes;
+
+*/
+
+Select * from Perfil
+
+INSERT INTO Perfil (USUARIO,CONTRASEÑA,NOMBRE,CORREO) VALUES ('admin','0000','H','hola@gmail.com');
+
+
+DELETE FROM [Manual].[dbo].[Perfil] WHERE ID = 247
