c3990855c6f6b901b414b7c48719903d92727904
Julio Vasquez <juliovas_96@hotmail.com>
2022-08-27 02:59:55 -0600

    material

--- Frontend/FrontEnd/src/app/app.module.ts
+++ Frontend/FrontEnd/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HamburgerMenuComponent } from './components/hamburger-menu/hamburger-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HamburgerMenuComponent } from './components/hamburger-menu/hamburger-me
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
