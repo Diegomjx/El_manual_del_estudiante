9cb251cfb5b466307a13039083db1125c0f9de5e
Diegomjx <19002145@galileo.edu>
2022-09-03 17:56:21 -0600

    Actualizar nombre

--- Frontend/FrontEnd/src/app/app.component.ts
+++ Frontend/FrontEnd/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterContentInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ComunicacionService } from './services/comunicacion.service';
 
 
 @Component({
@@ -7,21 +9,48 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent   {
   title = 'FrontEnd';
   panelOpenState = false;
   showAlert = false;
+  name = localStorage.getItem('name') || 'User';
 
-  constructor(private router:Router ) { }
+  constructor(
+    private serviceComunicate: ComunicacionService,
+    private router:Router ) { }
 
 
   login(){
     this.router.navigateByUrl('/login');
+    this.name = localStorage.getItem('name') || 'User';
   }
 
   upload(){
     this.router.navigateByUrl('/upload')
   }
+  ngOnInit(): void {
+    this.serviceComunicate.enviarnombreobservable.subscribe(nombre=>{
+      this.name = nombre;
+    })
+  }
+
+
+
+  ngOnChange(): void {
+    this.name = localStorage.getItem('name') || 'User';
+  }
+
+  Logout(){
+    localStorage.removeItem("id");
+        localStorage.removeItem("type");
+        localStorage.removeItem("name");
+        this.name = localStorage.getItem('name') || 'User';
+
+  }
+
+  
+
+
 
   
 }
--- Frontend/FrontEnd/src/app/components/login/login.component.ts
+++ Frontend/FrontEnd/src/app/components/login/login.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output, EventEmitter  } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { BackendService } from 'src/app/services/backend.service';
 import { userItem } from 'src/app/models/models';
 import { NgxToastService } from 'ngx-toast-notifier';
+import { ComunicacionService } from 'src/app/services/comunicacion.service';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,12 @@ export class LoginComponent implements OnInit {
     form: FormGroup;
     LoginUser: userItem[];
     userItem:  userItem;
+    name:string;
+
+    @Output() messageEvent = new EventEmitter<string>();
+
   constructor(private router:Router,
+              private serviceComunicate: ComunicacionService,
               private BackendService: BackendService,
               private fb: FormBuilder,
               private ngxToastService: NgxToastService) {
@@ -24,10 +30,11 @@ export class LoginComponent implements OnInit {
                 });
                 this.LoginUser = [];
                 this.userItem = this.LoginUser[0];
-
+                this.name = 'User';
                }
 
   ngOnInit(): void {
+    
   }
 
   getUser() {
@@ -41,13 +48,12 @@ export class LoginComponent implements OnInit {
         localStorage.setItem("type", "user");
         localStorage.setItem("name", this.userItem.USUARIO);
         //alert(this.userItem.ID);
+        this.serviceComunicate.enviarnombre(this.userItem.USUARIO);
         this.router.navigateByUrl('/');
         this.ngxToastService.onSuccess(' bienvenido',this.userItem.USUARIO);
       }
       else
       this.ngxToastService.onWarning('Denegado','Usuario o contraseña incorrecto');
-      
-         
     });
    // console.log(this.form.value);
   }
